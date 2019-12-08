e29d733306c033897759eab27a7fd3b4ca7c701f
Matthew J. Berger <matthewberger@nevada.unr.edu>
2019-12-07 19:11:17 -0800

    Add method for copying buffer to image.

--- src/resource/command_pool.rs
+++ src/resource/command_pool.rs
@@ -162,6 +162,48 @@ impl CommandPool {
         });
     }
 
+    pub fn copy_buffer_to_image(
+        &self,
+        transition_queue: vk::Queue,
+        buffer: vk::Buffer,
+        image: vk::Image,
+        width: u32,
+        height: u32,
+    ) {
+        self.execute_command_once(transition_queue, |command_buffer| {
+            let region = vk::BufferImageCopy::builder()
+                .buffer_offset(0)
+                .buffer_row_length(0)
+                .buffer_image_height(0)
+                .image_subresource(vk::ImageSubresourceLayers {
+                    aspect_mask: vk::ImageAspectFlags::COLOR,
+                    mip_level: 0,
+                    base_array_layer: 0,
+                    layer_count: 1,
+                })
+                .image_offset(vk::Offset3D { x: 0, y: 0, z: 0 })
+                .image_extent(vk::Extent3D {
+                    width,
+                    height,
+                    depth: 1,
+                })
+                .build();
+            let regions = [region];
+            unsafe {
+                self.context
+                    .logical_device()
+                    .logical_device()
+                    .cmd_copy_buffer_to_image(
+                        command_buffer,
+                        buffer,
+                        image,
+                        vk::ImageLayout::TRANSFER_DST_OPTIMAL,
+                        &regions,
+                    )
+            }
+        });
+    }
+
     // TODO: Refactor this to be smaller. Functionality can probably be reused
     // in generic command buffer submission method
     pub fn execute_command_once<F: FnOnce(vk::CommandBuffer)>(
