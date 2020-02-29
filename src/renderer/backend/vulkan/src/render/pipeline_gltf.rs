8f8fc754e6d06352b4a22cfb74a4baa2d7d7d3ee
Matthew J. Berger <matthewberger@nevada.unr.edu>
2020-02-28 23:11:48 -0800

    Refactor gltf pipeline creation.

--- src/renderer/backend/vulkan/src/render/pipeline_gltf.rs
+++ src/renderer/backend/vulkan/src/render/pipeline_gltf.rs
@@ -1,11 +1,12 @@
 use crate::{
+    core::VulkanContext,
     render::{gltf::VulkanGltfAsset, GraphicsPipeline, Renderer},
-    resource::{PipelineLayout, Shader},
+    resource::{DescriptorSetLayout, PipelineLayout, Shader},
 };
 use ash::{version::DeviceV1_0, vk};
 use nalgebra_glm as glm;
 use petgraph::{graph::NodeIndex, visit::Dfs};
-use std::{ffi::CString, mem, slice};
+use std::{ffi::CString, mem, slice, sync::Arc};
 pub struct PushConstantBlockMaterial {
     base_color_factor: glm::Vec4,
     color_texture_set: i32,
@@ -24,102 +25,25 @@ pub struct GltfPipeline {
 
 impl GltfPipeline {
     pub fn new(mut renderer: &mut Renderer, asset_names: &[String]) -> Self {
-        let shader_entry_point_name =
-            &CString::new("main").expect("Failed to create CString for shader entry point name!");
-
-        let vertex_shader = Shader::from_file(
-            renderer.context.clone(),
-            "examples/assets/shaders/shader.vert.spv",
-            vk::ShaderStageFlags::VERTEX,
-            shader_entry_point_name,
-        )
-        .unwrap();
-
-        let fragment_shader = Shader::from_file(
-            renderer.context.clone(),
-            "examples/assets/shaders/shader.frag.spv",
-            vk::ShaderStageFlags::FRAGMENT,
-            shader_entry_point_name,
-        )
-        .unwrap();
-
+        let (vertex_shader, fragment_shader, _shader_entry_point_name) =
+            Self::create_shaders(renderer.context.clone());
         let shader_state_info = [vertex_shader.state_info(), fragment_shader.state_info()];
 
-        let vertex_input_binding_description = vk::VertexInputBindingDescription::builder()
-            .binding(0)
-            .stride((8 * mem::size_of::<f32>()) as _)
-            .input_rate(vk::VertexInputRate::VERTEX)
-            .build();
-        let descriptions = [vertex_input_binding_description];
-
-        let position_description = vk::VertexInputAttributeDescription::builder()
-            .binding(0)
-            .location(0)
-            .format(vk::Format::R32G32B32_SFLOAT)
-            .offset(0)
-            .build();
-
-        let normal_description = vk::VertexInputAttributeDescription::builder()
-            .binding(0)
-            .location(1)
-            .format(vk::Format::R32G32B32_SFLOAT)
-            .offset((3 * mem::size_of::<f32>()) as _)
-            .build();
-
-        let tex_coord_description = vk::VertexInputAttributeDescription::builder()
-            .binding(0)
-            .location(2)
-            .format(vk::Format::R32G32_SFLOAT)
-            .offset((6 * mem::size_of::<f32>()) as _)
-            .build();
-
-        let attributes = [
-            position_description,
-            normal_description,
-            tex_coord_description,
-        ];
-
-        // Build vertex input creation info
+        let descriptions = Self::create_vertex_input_descriptions();
+        let attributes = Self::create_vertex_attributes();
         let vertex_input_create_info = vk::PipelineVertexInputStateCreateInfo::builder()
             .vertex_binding_descriptions(&descriptions)
             .vertex_attribute_descriptions(&attributes)
             .build();
 
-        // Build input assembly creation info
         let input_assembly_create_info = vk::PipelineInputAssemblyStateCreateInfo::builder()
             .topology(vk::PrimitiveTopology::TRIANGLE_LIST)
             .primitive_restart_enable(false)
             .build();
 
-        // Create a viewport
-        let viewport = vk::Viewport {
-            x: 0.0,
-            y: 0.0,
-            width: renderer
-                .vulkan_swapchain
-                .swapchain
-                .properties()
-                .extent
-                .width as _,
-            height: renderer
-                .vulkan_swapchain
-                .swapchain
-                .properties()
-                .extent
-                .height as _,
-            min_depth: 0.0,
-            max_depth: 1.0,
-        };
-        let viewports = [viewport];
-
-        // Create a stencil
-        let scissor = vk::Rect2D {
-            offset: vk::Offset2D { x: 0, y: 0 },
-            extent: renderer.vulkan_swapchain.swapchain.properties().extent,
-        };
-        let scissors = [scissor];
-
-        // Build the viewport info using the viewport and stencil
+        let extent = renderer.vulkan_swapchain.swapchain.properties().extent;
+        let viewports = Self::create_viewports(extent.width as _, extent.height as _);
+        let scissors = Self::create_scissors(extent);
         let viewport_create_info = vk::PipelineViewportStateCreateInfo::builder()
             .viewports(&viewports)
             .scissors(&scissors)
@@ -139,7 +63,6 @@ impl GltfPipeline {
             .depth_bias_slope_factor(0.0)
             .build();
 
-        // Create the multisampling info for the pipline
         let multisampling_create_info = vk::PipelineMultisampleStateCreateInfo::builder()
             .sample_shading_enable(false)
             .rasterization_samples(vk::SampleCountFlags::TYPE_1)
@@ -161,20 +84,7 @@ impl GltfPipeline {
             .back(Default::default())
             .build();
 
-        // Create the color blend attachment
-        let color_blend_attachment = vk::PipelineColorBlendAttachmentState::builder()
-            .color_write_mask(vk::ColorComponentFlags::all())
-            .blend_enable(false)
-            .src_color_blend_factor(vk::BlendFactor::ONE)
-            .dst_color_blend_factor(vk::BlendFactor::ZERO)
-            .color_blend_op(vk::BlendOp::ADD)
-            .src_alpha_blend_factor(vk::BlendFactor::ONE)
-            .dst_alpha_blend_factor(vk::BlendFactor::ZERO)
-            .alpha_blend_op(vk::BlendOp::ADD)
-            .build();
-        let color_blend_attachments = [color_blend_attachment];
-
-        // Build the color blending info using the color blend attachment
+        let color_blend_attachments = Self::create_color_blend_attachments();
         let color_blending_info = vk::PipelineColorBlendStateCreateInfo::builder()
             .logic_op_enable(false)
             .logic_op(vk::LogicOp::COPY)
@@ -184,18 +94,8 @@ impl GltfPipeline {
 
         let descriptor_set_layout =
             VulkanGltfAsset::descriptor_set_layout(renderer.context.clone());
-        let descriptor_set_layouts = [descriptor_set_layout.layout()];
-        let push_constant_range = vk::PushConstantRange::builder()
-            .stage_flags(vk::ShaderStageFlags::ALL_GRAPHICS)
-            .size(mem::size_of::<PushConstantBlockMaterial>() as u32)
-            .build();
-        let push_constant_ranges = [push_constant_range];
-        let pipeline_layout_create_info = vk::PipelineLayoutCreateInfo::builder()
-            .set_layouts(&descriptor_set_layouts) // needed for uniforms in shaders
-            .push_constant_ranges(&push_constant_ranges)
-            .build();
         let pipeline_layout =
-            PipelineLayout::new(renderer.context.clone(), pipeline_layout_create_info);
+            Self::create_pipeline_layout(renderer.context.clone(), &descriptor_set_layout);
 
         // Create the pipeline info
         let pipeline_create_info = vk::GraphicsPipelineCreateInfo::builder()
@@ -232,6 +132,121 @@ impl GltfPipeline {
         let gltf_pipeline = Self { pipeline, assets };
         gltf_pipeline.create_gltf_render_passes(&mut renderer);
         gltf_pipeline
+    }
+
+    fn create_shaders(context: Arc<VulkanContext>) -> (Shader, Shader, CString) {
+        let shader_entry_point_name =
+            CString::new("main").expect("Failed to create CString for shader entry point name!");
+
+        let vertex_shader = Shader::from_file(
+            context.clone(),
+            "examples/assets/shaders/shader.vert.spv",
+            vk::ShaderStageFlags::VERTEX,
+            &shader_entry_point_name,
+        )
+        .expect("Failed to create vertex shader!");
+
+        let fragment_shader = Shader::from_file(
+            context,
+            "examples/assets/shaders/shader.frag.spv",
+            vk::ShaderStageFlags::FRAGMENT,
+            &shader_entry_point_name,
+        )
+        .expect("Failed to create fragment shader!");
+
+        (vertex_shader, fragment_shader, shader_entry_point_name)
+    }
+
+    fn create_vertex_input_descriptions() -> [vk::VertexInputBindingDescription; 1] {
+        let vertex_input_binding_description = vk::VertexInputBindingDescription::builder()
+            .binding(0)
+            .stride((8 * mem::size_of::<f32>()) as _)
+            .input_rate(vk::VertexInputRate::VERTEX)
+            .build();
+        [vertex_input_binding_description]
+    }
+
+    fn create_vertex_attributes() -> [vk::VertexInputAttributeDescription; 3] {
+        let position_description = vk::VertexInputAttributeDescription::builder()
+            .binding(0)
+            .location(0)
+            .format(vk::Format::R32G32B32_SFLOAT)
+            .offset(0)
+            .build();
+
+        let normal_description = vk::VertexInputAttributeDescription::builder()
+            .binding(0)
+            .location(1)
+            .format(vk::Format::R32G32B32_SFLOAT)
+            .offset((3 * mem::size_of::<f32>()) as _)
+            .build();
+
+        let tex_coord_description = vk::VertexInputAttributeDescription::builder()
+            .binding(0)
+            .location(2)
+            .format(vk::Format::R32G32_SFLOAT)
+            .offset((6 * mem::size_of::<f32>()) as _)
+            .build();
+
+        [
+            position_description,
+            normal_description,
+            tex_coord_description,
+        ]
+    }
+
+    pub fn create_viewports(width: f32, height: f32) -> [vk::Viewport; 1] {
+        let viewport = vk::Viewport {
+            x: 0.0,
+            y: 0.0,
+            width,
+            height,
+            min_depth: 0.0,
+            max_depth: 1.0,
+        };
+        [viewport]
+    }
+
+    pub fn create_scissors(extent: vk::Extent2D) -> [vk::Rect2D; 1] {
+        let scissor = vk::Rect2D {
+            offset: vk::Offset2D { x: 0, y: 0 },
+            extent,
+        };
+        [scissor]
+    }
+
+    pub fn create_color_blend_attachments() -> [vk::PipelineColorBlendAttachmentState; 1] {
+        let color_blend_attachment = vk::PipelineColorBlendAttachmentState::builder()
+            .color_write_mask(vk::ColorComponentFlags::all())
+            .blend_enable(false)
+            .src_color_blend_factor(vk::BlendFactor::ONE)
+            .dst_color_blend_factor(vk::BlendFactor::ZERO)
+            .color_blend_op(vk::BlendOp::ADD)
+            .src_alpha_blend_factor(vk::BlendFactor::ONE)
+            .dst_alpha_blend_factor(vk::BlendFactor::ZERO)
+            .alpha_blend_op(vk::BlendOp::ADD)
+            .build();
+        [color_blend_attachment]
+    }
+
+    pub fn create_pipeline_layout(
+        context: Arc<VulkanContext>,
+        descriptor_set_layout: &DescriptorSetLayout,
+    ) -> PipelineLayout {
+        let descriptor_set_layouts = [descriptor_set_layout.layout()];
+
+        let push_constant_range = vk::PushConstantRange::builder()
+            .stage_flags(vk::ShaderStageFlags::ALL_GRAPHICS)
+            .size(mem::size_of::<PushConstantBlockMaterial>() as u32)
+            .build();
+        let push_constant_ranges = [push_constant_range];
+
+        let pipeline_layout_create_info = vk::PipelineLayoutCreateInfo::builder()
+            .set_layouts(&descriptor_set_layouts) // needed for uniforms in shaders
+            .push_constant_ranges(&push_constant_ranges)
+            .build();
+
+        PipelineLayout::new(context, pipeline_layout_create_info)
     }
 
     pub fn create_gltf_render_passes(&self, renderer: &mut Renderer) {
