2b2393fab0cf493d4002163a7288e0800f8a99f6
Matthew J. Berger <matthewberger@nevada.unr.edu>
2020-05-07 07:23:14 -0700

    Start adding alpha blending.

--- src/renderer/backend/vulkan/src/pipelines/pbr.rs
+++ src/renderer/backend/vulkan/src/pipelines/pbr.rs
@@ -155,9 +155,9 @@ impl PbrPipeline {
     pub fn create_color_blend_attachments() -> [vk::PipelineColorBlendAttachmentState; 1] {
         let color_blend_attachment = vk::PipelineColorBlendAttachmentState::builder()
             .color_write_mask(vk::ColorComponentFlags::all())
-            .blend_enable(false)
-            .src_color_blend_factor(vk::BlendFactor::ONE)
-            .dst_color_blend_factor(vk::BlendFactor::ZERO)
+            .blend_enable(true)
+            .src_color_blend_factor(vk::BlendFactor::SRC_ALPHA)
+            .dst_color_blend_factor(vk::BlendFactor::ONE_MINUS_SRC_ALPHA)
             .color_blend_op(vk::BlendOp::ADD)
             .src_alpha_blend_factor(vk::BlendFactor::ONE)
             .dst_alpha_blend_factor(vk::BlendFactor::ZERO)
